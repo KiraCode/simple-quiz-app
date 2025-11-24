82bc839547b00e0e533fc4f25b65e2f01ca82964
tejas git <d9489079@gmail.com>
2025-11-24 22:45:22 +0530

    validating the answer on question

--- backend/controllers/questionController.js
+++ backend/controllers/questionController.js
@@ -17,4 +17,39 @@ const getQuestions = async (req, res) => {
   }
 };
 
-module.exports = { getQuestions };
+const addAnswer = async (req, res) => {
+  try {
+    // optionid & value & questionId
+    const { qId, answer } = req.body;
+
+    if (!qId || !answer) {
+      return res.status(400).json({
+        success: false,
+        message: "Bad request - need qId and answer",
+      });
+    }
+
+    const questions = await Question.findById(qId);
+    if (!questions) {
+      return res.status(400).json({
+        success: false,
+        message: "No Question Found",
+      });
+    }
+
+    const isCorrect = questions.answer.id === answer.id;
+
+    return res.status(200).json({
+      success: true,
+      status: isCorrect ? 1 : 0,
+      message: `${isCorrect ? "correct" : "wrong"} answer ${
+        isCorrect ? "😁" : "😣"
+      }`,
+    });
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({ success: false, message: "Server error" });
+  }
+};
+
+module.exports = { getQuestions, addAnswer };
--- backend/routes/questionRoutes.js
+++ backend/routes/questionRoutes.js
@@ -1,8 +1,12 @@
 const express = require("express");
-const { getQuestions } = require("../controllers/questionController.js");
+const {
+  getQuestions,
+  addAnswer,
+} = require("../controllers/questionController.js");
 
 const questionRouter = express.Router();
 
 questionRouter.get("/", getQuestions);
+questionRouter.post("/validate-answer", addAnswer);
 
 module.exports = questionRouter;
